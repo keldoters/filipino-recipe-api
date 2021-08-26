39c33e685ba9dfb1ceb871f268ec3ac234cdcff3
keldoters <35092544+keldoters@users.noreply.github.com>
2021-08-26 15:40:41 +0800

    initial commit

---
+++ src/main/java/org/keldoters/pinoyrecipesapi/PinoyRecipesApiApplication.java
@@ -0,0 +1,13 @@
+package org.keldoters.pinoyrecipesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinoyRecipesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinoyRecipesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/keldoters/pinoyrecipesapi/PinoyRecipesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.keldoters.pinoyrecipesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinoyRecipesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
